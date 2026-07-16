72d0e4c55b479a670ecd8d0121c5d648cdf9fa94
zahoorulislam <68445944+zahoorulislam@users.noreply.github.com>
2026-07-17 02:39:57 +0400

    Delete create_directories.sh

--- create_directories.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p data/raw data/processed data/external
-mkdir -p notebooks src outputs/figures outputs/tables outputs/models docs reports
-touch data/raw/.gitkeep data/processed/.gitkeep data/external/.gitkeep
-touch outputs/figures/.gitkeep outputs/tables/.gitkeep outputs/models/.gitkeep
-echo "GitHub directory structure created."
